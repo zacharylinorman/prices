531ce10b8451eefe83d22bc3e028ed8a4ba7897b
Zachary Li Norman <zacharylinorman@gmail.com>
2020-08-07 00:17:25 -0700

    Updates

---
+++ precache-manifest.51b83bba518617dec0dcecb142a35c78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed5203dcc2bd59d91c437bea6c0c5c55",
+    "url": "/prices/index.html"
+  },
+  {
+    "revision": "3bf80455777961fb1497",
+    "url": "/prices/static/css/main.68fa013b.chunk.css"
+  },
+  {
+    "revision": "f455fbe3788e715f141f",
+    "url": "/prices/static/js/2.86f70f61.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/prices/static/js/2.86f70f61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bf80455777961fb1497",
+    "url": "/prices/static/js/main.7ecc830b.chunk.js"
+  },
+  {
+    "revision": "8c0ba119a794ca070e8c",
+    "url": "/prices/static/js/runtime-main.ef76c6a9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/prices/static/media/logo.5d5d9eef.svg"
+  }
+]);
